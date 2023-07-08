1ecbbce948eeccb69f2cfcd2f76f1e3cf793f32f
Brycenbeans <bryceblacksoftware@gmail.com>
2023-07-08 16:13:17 -0700

    Added Scriptable Mail Objects

--- Assets/Main/MailScript.cs
+++ Assets/Main/MailScript.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class MailScript : MonoBehaviour
 {
+    MailScriptableObject mailScript;
+    private bool pointsGiven = false;
+
+    public string MailName;
+    public float MailMass;
+    public float MailSpeed;
+    public int MailPoints;
+
+    FirstPersonController firstPersonController;
+
+    private void Start()
+    {
+        MailName = mailScript.mailName;
+        MailMass = mailScript.mailMass;
+        MailSpeed = mailScript.mailSpeed;
+        MailPoints = mailScript.mailPoints;
+        firstPersonController.ChangeMailInfo(MailName, MailSpeed);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.tag == "Boundry")
         {
             Destroy(this.gameObject);
         }
-        //test
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
     }
 }
